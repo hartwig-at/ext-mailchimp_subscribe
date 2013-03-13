a6dd8759e171bc7c3d847955b5696b9a59184432
Oliver Salzburg <oliver.salzburg@gmail.com>
2013-03-13 18:50:17 +0100

    Made core methods uncachable again

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 if( !defined( 'TYPO3_MODE' ) ) {
 	die( 'Access denied.' );
 }
-Tx_Extbase_Utility_Extension::configurePlugin(  'mailchimp_subscribe',  'Form',  array(    'Form' => 'display,subscribe,update,onPowermailSubmit'  ),  array(    'Form' => ''  ));
+Tx_Extbase_Utility_Extension::configurePlugin(  'mailchimp_subscribe',  'Form',  array(    'Form' => 'onPowermailSubmit'  ),  array(    'Form' => 'display,subscribe,update'  ));
 $signalSlotDispatcher = t3lib_div::makeInstance('Tx_Extbase_SignalSlot_Dispatcher');
 $signalSlotDispatcher->connect( 'Tx_Powermail_Controller_FormsController', 'createActionAfterSubmitView', 'Tx_MailchimpSubscribe_Controller_FormController', 'onPowermailSubmitAction', FALSE );
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['Tx_MailchimpSubscribe_Tasks_ResetSubscriptionsTask'] = array(	'extension'        => 'mailchimp_subscribe',	'title'            => 'LLL:EXT:mailchimp_subscribe/Resources/Private/Language/locallang_be.xml:resetsubscriptions.name',	'description'      => 'LLL:EXT:mailchimp_subscribe/Resources/Private/Language/locallang_be.xml:resetsubscriptions.description','additionalFields' => 'tx_mailchimpsubscribe_tasks_resetsubscriptionstaskfields');
